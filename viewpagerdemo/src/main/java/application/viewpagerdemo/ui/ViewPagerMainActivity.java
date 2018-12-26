5619805652d478270a20c04e9a9e6f06d125171e
crazysniper <1163432768@qq.com>
2018-12-26 17:31:57 +0800

    11

--- viewpagerdemo/src/main/java/application/viewpagerdemo/ui/ViewPagerMainActivity.java
+++ viewpagerdemo/src/main/java/application/viewpagerdemo/ui/ViewPagerMainActivity.java
@@ -1,11 +1,15 @@
 package application.viewpagerdemo.ui;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.widget.Button;
 
+import com.alibaba.android.arouter.facade.annotation.Route;
+import com.alibaba.android.arouter.launcher.ARouter;
 import com.myjoke.baselibray.base.BaseActivity;
 
+import application.viewpagerdemo.R;
+import application.viewpagerdemo.util.ViewPagerConstant;
+
+@Route(path = ViewPagerConstant.ViewPagerMainActivity)
 public class ViewPagerMainActivity extends BaseActivity {
 
     private Button btn1;
@@ -17,6 +21,7 @@ public class ViewPagerMainActivity extends BaseActivity {
 
     @Override
     public void initView() {
+        ARouter.getInstance().inject(this);
         btn1 = findView(R.id.btn1);
     }
 
--- viewpagerdemo/src/main/java/application/viewpagerdemo/ui/ViewPager_View_Activity.java
+++ viewpagerdemo/src/main/java/application/viewpagerdemo/ui/ViewPager_View_Activity.java
@@ -1,22 +1,33 @@
 package application.viewpagerdemo.ui;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
 
+import com.alibaba.android.arouter.facade.annotation.Route;
+import com.alibaba.android.arouter.launcher.ARouter;
 import com.myjoke.baselibray.base.BaseActivity;
 
+import application.viewpagerdemo.R;
+import application.viewpagerdemo.util.ViewPagerConstant;
+
+@Route(path = ViewPagerConstant.ViewPager_View_Activity)
 public class ViewPager_View_Activity extends BaseActivity {
 
 
     @Override
     public int getLayoutId() {
-        return R.layout.activity_view_pager;
+        return R.layout.activity_view_pager_view;
     }
 
     @Override
     public void initView() {
-        LayoutInflater layoutInflater = LayoutInflater.from(ViewPager_View_Activity.this);
+        ARouter.getInstance().inject(this);
+        LayoutInflater layoutInflater = getLayoutInflater();
+
+        View view1 = layoutInflater.inflate(R.layout.item, null);
+        TextView textView= view1.findViewById(R.id.textView);
+
     }
 
     @Override
