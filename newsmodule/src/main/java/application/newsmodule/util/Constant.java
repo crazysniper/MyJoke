317d395befb759894cf83d8dfabf8d6ce425d830
crazysniper <1163432768@qq.com>
2019-01-23 14:24:21 +0800

    11

---
+++ newsmodule/src/main/java/application/newsmodule/util/Constant.java
@@ -0,0 +1,12 @@
+package application.newsmodule.util;
+
+/**
+ * Created by Gao on 2019/1/23.
+ */
+
+public class Constant {
+    public static final String APP_KEY = "";
+    public static final String APP_SECRET = "";
+    public static final String APP_CODE = "";
+
+}
