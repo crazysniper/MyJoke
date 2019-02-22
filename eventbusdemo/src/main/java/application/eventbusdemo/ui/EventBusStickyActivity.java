9075d3fcc871fe95430d3dd21c469c312f2f23a1
crazysniper <1163432768@qq.com>
2019-02-22 15:14:46 +0800

    22

--- eventbusdemo/src/main/java/application/eventbusdemo/ui/EventBusStickyActivity.java
+++ eventbusdemo/src/main/java/application/eventbusdemo/ui/EventBusStickyActivity.java
@@ -44,31 +44,29 @@ public class EventBusStickyActivity extends BaseActivity {
     protected void register() {
     }
 
-    @OnClick({R2.id.register, R2.id.unregister})
+    @OnClick({R2.id.register, R2.id.unregister, R2.id.removeSticky, R2.id.getStickyEvent})
     public void onClick(View view) {
         if (view.getId() == R.id.register) {
             if (!EventBus.getDefault().isRegistered(this)) {
                 EventBus.getDefault().register(this);
                 ToastUtil.getInstance().showToast("绑定");
             }
-        }else if (view.getId() == R.id.unregister) {
+        } else if (view.getId() == R.id.unregister) {
             if (EventBus.getDefault().isRegistered(this)) {
                 EventBus.getDefault().unregister(this);
                 ToastUtil.getInstance().showToast("解绑");
             }
+        } else if (view.getId() == R.id.removeSticky) {
+            EventBus.getDefault().removeAllStickyEvents();
+        } else if (view.getId() == R.id.getStickyEvent) {
+            EventBean eventBean = EventBus.getDefault().getStickyEvent(EventBean.class);
         }
     }
 
     @Subscribe(threadMode = ThreadMode.MAIN, sticky = true)
     public void main(EventBean eventBean) {
         LogUtil.e("main=" + eventBean);
-        EventBus.getDefault().removeStickyEvent(eventBean);
-
-
-        EventBus.getDefault().removeAllStickyEvents();
-
-
-        EventBus.getDefault().getStickyEvent(EventBean.class);
+        boolean result = EventBus.getDefault().removeStickyEvent(eventBean);
     }
 
     @Subscribe(threadMode = ThreadMode.BACKGROUND, sticky = true)
